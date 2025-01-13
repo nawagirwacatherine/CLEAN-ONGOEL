4de11447320fc64957d3577a797ed2ae637dc2ce
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-01-13 07:28:24 +0200

    Added routing to the app

--- clean-ongoel/src/App.jsx
+++ clean-ongoel/src/App.jsx
@@ -1,37 +1,36 @@
-import "./App.css"
-import Home from"./pages/Home.jsx"
-import AboutUs from"./pages/AboutUs.jsx"
-import GetInvolved from "./pages/GetInvolved.jsx"
-import Projects from"./pages/Projects.jsx"
-import News from"./pages/News.jsx"
-import Contact from"./pages/Contact.jsx"
-import { Routes, Route, Link } from 'react-router-dom';
 
+import "./App.css";
+import Home from "./pages/Home.jsx";
+import AboutUs from "./pages/AboutUs.jsx";
+import GetInvolved from "./pages/GetInvolved.jsx";
+import Projects from "./pages/Projects.jsx";
+import News from "./pages/News.jsx";
+import Contact from "./pages/Contact.jsx";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
 
-function  App () {
-   return (
-    <>
-    <div>
-    <Link to="/" className="nav-item">Home</Link>
-    <Link to="/About-us" className="nav-item">About us</Link>
-    <Link to="/get-involved" className="nav-item">GetInvolved</Link>
-    <Link to="/projects" className="nav-item">Projects</Link>
-    <Link to="/news" className="nav-item">News</Link>
-    <Link to="/contact" className="nav-item">Contact</Link>
+function App() {
+  return (
 
-   
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About-us" element={<AboutUs />} />
-        <Route path="/getinvolved" element={< GetInvolved/>} />
-        <Route path="/projects" element={< Projects />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/contacts" element={<Contact />} />
-      </Routes>
-      </div>
-    </>
-
-   )
+    <BrowserRouter>
+    
+        <nav className="nav">
+          <Link to="/" className="nav-item">Home</Link>
+          <Link to="/about-us" className="nav-item">About Us</Link>
+          <Link to="/get-involved" className="nav-item">Get Involved</Link>
+          <Link to="/projects" className="nav-item">Projects</Link>
+          <Link to="/news" className="nav-item">News</Link>
+          <Link to="/contact" className="nav-item">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/get-involved" element={<GetInvolved />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
