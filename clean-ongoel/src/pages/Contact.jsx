641bdfcdc90ef176c1b1b64817ec21110a40275e
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-01-23 16:36:58 +0200

    added an item in contacts page

--- clean-ongoel/src/pages/Contact.jsx
+++ clean-ongoel/src/pages/Contact.jsx
@@ -7,9 +7,15 @@ function  Contact () {
 
           <div  className="recent-news1">
               <div  className="recent-newsblood">
-              <h1>For bible study</h1>
+              <h1>For bible course and study</h1>
               <p className="to-form">Please fill the form below.< br /> we shall contact you or reachout to our different branches </p>
-             
+              <form action="">
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" />
+
+                <label htmlFor="contact">contacr</label>
+                <input type="contact" id="contact" />
+              </form>
              </div>
              <div  className="recent-newshealth">
              <h1>For other activities</h1>
