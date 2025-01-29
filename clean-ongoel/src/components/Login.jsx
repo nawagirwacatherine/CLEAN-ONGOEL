f7031ff370ca0878149c095fb6916480a2a2ed6d
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-01-29 07:48:18 +0200

    add the logout button

--- clean-ongoel/src/components/Login.jsx
+++ clean-ongoel/src/components/Login.jsx
@@ -2,21 +2,30 @@ import { useState } from "react";
 
 function Login (prop) {
 
-  const [ loginForm, setLoginForm] = useState();
-  const [ registerForm , setRegisterForm]  = useState();
+  const [ loginForm, setLoginForm] = useState(false);
+  const [ registerForm , setRegisterForm]  = useState(false);
   const [activity, setActivity] = useState("choose activity");
+  const [formData, setFormData] = useState({name:"", contact:""})
 
   const handleLoginClick  = () => {
     setLoginForm(true);
     setRegisterForm(false);
   };
 
-  const handleRegisterClick = () => {
+ 
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevents the default form submission behavior
+    console.log("Form Submitted:", formData, activity); // Debugging or further actions
+    alert("Form submitted successfully!");
+    setFormData({ name: "", contact: "" });
+  };
+
+  const handleRegisterClick= () => {
     setLoginForm (false);
-    setRegisterForm(true);
+  
 
   }
-
 
 
     return(
@@ -24,16 +33,17 @@ function Login (prop) {
 
    <div className="form">
     <button className="login" onClick={handleLoginClick}>{prop.name}</button>
-
+    <button  className="register" onClick={handleRegisterClick}>Logout</button>
 
     { loginForm  && (
 
       <div className="login-form">
-        <form>
+        <form onSubmit={handleSubmit}>
         <label htmlFor="activity">
          activity:
        
-        <select  id="activity"
+        <select  
+        id="activity"
         value={activity}
         onChange={(e) => setActivity(e.target.value)}
         >
@@ -53,15 +63,24 @@ function Login (prop) {
       
         <br />
         <label>
-         name:
+         Name:
          
-          <input type="name" placeholder ="Enter your name" />
+          <input
+           type="name" 
+           placeholder ="Enter your name"
+           value={formData.name} 
+           onChange={(e) => setFormData ({...formData, name: e.target.value})}/>
         </label>
         <br />
 
         <label>
           contact:
-          <input type="contact" placeholder="Enter your contact"/>
+          <input type="contact" 
+          placeholder="Enter your contact"
+          value= {formData.contact}
+          onChange= {(e) =>
+            setFormData({...formData,contact: e.target.value})
+          }/>
         </label>
         <br />
 
